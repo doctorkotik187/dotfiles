f3223ea70880616de6ac9856e2b7713812502435
doctorkotik187 <217476020+doctorkotik187@users.noreply.github.com>
2025-12-06 15:23:44 +0100

    switched to nushell!

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- fix window buttons
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-
 -- font
 config.font = wezterm.font 'BlexMono Nerd Font Medium'
 config.font_size = 10
@@ -14,5 +11,8 @@ config.color_scheme = 'Tokyo Night'
 -- multiplexing
 config.unix_domains = {{ name = 'unix' }}
 config.default_gui_startup_args = { 'connect', 'unix' }
+
+-- nushell
+config.default_prog = { '/home/linuxbrew/.linuxbrew/bin/nu' }
 
 return config
