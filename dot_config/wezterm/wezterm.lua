4399a779d2ef39b6f73c587b9fb46fb699706193
doctorkotik187 <217476020+doctorkotik187@users.noreply.github.com>
2026-02-20 17:46:39 +0100

    small tweaks

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,8 +11,8 @@ config.mouse_bindings = {
 }
 
 -- size
--- config.initial_cols = 120
--- config.initial_rows = 30
+config.initial_cols = 120
+config.initial_rows = 30
 
 -- font
 config.font = wezterm.font 'Maple Mono NF Medium'
