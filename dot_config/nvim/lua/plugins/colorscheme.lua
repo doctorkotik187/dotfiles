290b4448248c2d4a3b9fc498b5952f42422883ab
doctorkotik187 <217476020+doctorkotik187@users.noreply.github.com>
2026-08-24 17:35:31 +0900

    ricing lazyvim and herdr fresh

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,4 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "macchiato" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+}
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 ---- MOUSE ---------------------------------------------------
 
 -- stop mouse clicks from auto-opening urls
 config.mouse_bindings = {
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'NONE',
-    action = wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection',
-  },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
 }
 
 ---- WINDOW --------------------------------------------------
@@ -19,15 +19,15 @@ config.initial_rows = 30
 
 ---- FONT ----------------------------------------------------
 
-config.font = wezterm.font('Maple Mono NF CN', { weight = 'Medium' })
+config.font = wezterm.font("Maple Mono NF CN", { weight = "Medium" })
 config.font_size = 9
 
 ---- THEME & VISUALS -----------------------------------------
 
--- config.color_scheme = 'Catppuccin Latte'     -- light
-config.color_scheme = 'Catppuccin Macchiato'    -- dark
+-- config.color_scheme = "Catppuccin Latte"     -- light
+config.color_scheme = "Catppuccin Macchiato" -- dark
 
-config.use_fancy_tab_bar = false                 -- minimal tab bar
-config.hide_tab_bar_if_only_one_tab = true       -- hide tab bar when only one tab
+config.use_fancy_tab_bar = false -- minimal tab bar
+config.hide_tab_bar_if_only_one_tab = true -- hide tab bar when only one tab
 
 return config
